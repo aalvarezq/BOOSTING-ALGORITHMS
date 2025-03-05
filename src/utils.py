c6306921bd999bb82e652acf57e97f2f54dac1c6
Aldo <157986984+aalvarezq@users.noreply.github.com>
2025-03-05 13:31:31 +0000

    terminado

--- src/utils.py
+++ src/utils.py
@@ -1,13 +1,29 @@
-from dotenv import load_dotenv
-from sqlalchemy import create_engine
 import pandas as pd
+import numpy as np
 
-# load the .env file variables
-load_dotenv()
+df = pd.read_csv('https://raw.githubusercontent.com/4GeeksAcademy/decision-tree-project-tutorial/main/diabetes.csv')
+df = df.drop_duplicates().reset_index(drop=True)
+
+# Lista de columnas
+columns = df.columns
 
 
-def db_connect():
-    import os
-    engine = create_engine(os.getenv('DATABASE_URL'))
-    engine.connect()
-    return engine
+#Tratamiento de caracteristicas
+
+df[['Pregnancies', 'Glucose', 'BloodPressure', 'SkinThickness', 'Insulin', 'BMI', 'DiabetesPedigreeFunction', 'Age']] = df[['Pregnancies', 'Glucose', 'BloodPressure', 'SkinThickness', 'Insulin', 'BMI', 'DiabetesPedigreeFunction', 'Age']].replace(0, np.nan)
+
+def mediana_target(var):   
+    temp = df[df[var].notnull()]
+    temp = temp[[var, 'Outcome']].groupby(['Outcome'])[[var]].median().reset_index()
+    return temp
+
+columns = df.columns
+columns = columns.drop("Outcome")
+for i in columns:
+    mediana_target(i)
+    df.loc[(df['Outcome'] == 0 ) & (df[i].isnull()), i] = mediana_target(i)[i][0]
+    df.loc[(df['Outcome'] == 1 ) & (df[i].isnull()), i] = mediana_target(i)[i][1]
+
+df.isnull().sum()
+
+print(df.describe())
